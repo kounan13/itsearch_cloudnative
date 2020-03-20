e1ebe84edc0410384c2041bf6dce82d55addfc7e
kounan13 <yujishono@hotmail.com>
2020-03-21 02:17:51 +0900

    initial commit

---
+++ samplemaven/src/main/java/org/debugroom/mynavi/sample/aws/lambda/functions/ApiGatewayEventFunction.java
@@ -0,0 +1,31 @@
+package org.debugroom.mynavi.sample.aws.lambda.functions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
+@Slf4j
+public class ApiGatewayEventFunction implements Function<Message<Input>, Message<String>> {
+
+    @Override
+    public Message<String> apply(Message<Input> inputMessage) {
+        log.info("Message : " + inputMessage.getPayload().getTest());
+        return new Message<String>() {
+            @Override
+            public String getPayload() {
+                return "Complete!";
+            }
+
+            @Override
+            public MessageHeaders getHeaders() {
+                Map<String, Object> headers = new HashMap<>();
+                return new MessageHeaders(headers);
+            }
+        };
+    }
+ }
---
+++ samplemaven/src/main/java/org/debugroom/mynavi/sample/aws/lambda/functions/Input.java
@@ -0,0 +1,8 @@
+package org.debugroom.mynavi.sample.aws.lambda.functions;
+
+import lombok.Data;
+@Data
+public class Input{
+
+    private String test;
+}
